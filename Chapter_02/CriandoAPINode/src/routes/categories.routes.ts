8bfc9c06611cf865d30c98eaa8c24882bb3ed2e2
RudhyDev <rudhy.pereira@gmail.com>
2022-05-23 23:13:38 -0300

    feat: create CreateCategoryService

--- Chapter_02/CriandoAPINode/src/routes/categories.routes.ts
+++ Chapter_02/CriandoAPINode/src/routes/categories.routes.ts
@@ -1,3 +1,4 @@
+import { CreateCategoryService } from './../services/CreateCategoryService';
 import { CategoriesRepository } from "./../repositories/CategoriesRepository";
 import { Router } from "express";
 
@@ -8,13 +9,8 @@ const categoriesRepository = new CategoriesRepository();
 categoriesRoutes.post("/", (req, res) => {
   const { name, description } = req.body;
 
-  const categoryAlreadyExists = categoriesRepository.findByName(name);
-
-  if (categoryAlreadyExists) {
-    return res.status(400).json({error: 'Category already exists'});
-  }
-
-  categoriesRepository.create({ name, description });
+  const createCategoryService = new CreateCategoryService(categoriesRepository);
+  createCategoryService.execute({ name, description });
 
   return res.status(201).send();
 });
